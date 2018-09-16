9cd83b2ad0274afa5e45faa785f7f9258bdd9cbb
raz ohad <admin@bainternet.info>
2018-09-16 19:52:24 +0300

    grunt webpack eslint

---
+++ Gruntfile.js
@@ -0,0 +1,129 @@
+module.exports = function( grunt ) {
+	'use strict';
+
+	const fs = require( 'fs' ),
+		pkgInfo = grunt.file.readJSON( 'package.json' );
+
+	require( 'load-grunt-tasks' )( grunt );
+
+	// Project configuration
+	grunt.initConfig( {
+		pkg: pkgInfo,
+
+		banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+				'<%= grunt.template.today("dd-mm-yyyy") %> */',
+
+		checktextdomain: require( './.grunt-config/checktextdomain' ),
+		usebanner: require( './.grunt-config/usebanner' ),
+		sass: require( './.grunt-config/sass' ),
+		postcss: require( './.grunt-config/postcss' ),
+		watch: require( './.grunt-config/watch' ),
+		wp_readme_to_markdown: require( './.grunt-config/wp_readme_to_markdown' ),
+		bumpup: require( './.grunt-config/bumpup' ),
+		replace: require( './.grunt-config/replace' ),
+		shell: require( './.grunt-config/shell' ),
+		release: require( './.grunt-config/release' ),
+		copy: require( './.grunt-config/copy' ),
+		clean: require( './.grunt-config/clean' ),
+		webpack: require( './.grunt-config/webpack' ),
+		qunit: {
+			src: 'tests/qunit/index.html',
+		},
+	} );
+
+	// Default task(s).
+	grunt.registerTask( 'default', [
+		'i18n',
+		'wp_readme_to_markdown',
+		'scripts',
+		'styles',
+	] );
+
+	grunt.registerTask( 'i18n', [
+		'checktextdomain',
+	] );
+
+	grunt.registerTask( 'scripts', ( isDevMode = false ) => {
+		let taskName = 'webpack:production';
+
+		if ( isDevMode ) {
+			taskName = 'webpack:development';
+		}
+
+		grunt.task.run( taskName );
+	} );
+
+	grunt.registerTask( 'watch_scripts', () => {
+		grunt.task.run( 'webpack:development' );
+	} );
+
+	grunt.registerTask( 'styles', ( isDevMode = false ) => {
+		grunt.task.run( 'sass' );
+
+		if ( ! isDevMode ) {
+			grunt.task.run( 'postcss' );
+			grunt.task.run( 'css_templates' );
+		}
+	} );
+
+	grunt.registerTask( 'watch_styles', () => {
+		grunt.task.run( 'watch:styles' );
+	} );
+
+	grunt.registerTask( 'css_templates', () => {
+		grunt.task.run( 'css_templates_proxy:templates' );
+
+		grunt.config( 'sass.dist', {
+			files: [ {
+				expand: true,
+				cwd: 'assets/dev/scss/direction',
+				src: [ 'frontend.scss', 'frontend-rtl.scss' ],
+				dest: 'assets/css/templates',
+				ext: '.css',
+			} ],
+		} );
+
+		grunt.task.run( 'sass' );
+
+		grunt.config( 'postcss.minify.files.0.src', [
+			'assets/css/templates/*.css',
+			'!assets/css/templates/*.min.css',
+		] );
+
+		grunt.task.run( 'postcss:minify' );
+
+		grunt.task.run( 'css_templates_proxy:values' );
+	} );
+
+	// Writing the proxy file as a grunt task, in order to fit in with the tasks queue
+	grunt.registerTask( 'css_templates_proxy', ( mode ) => {
+		fs.writeFileSync( 'assets/dev/scss/frontend/breakpoints/proxy.scss', '@import "' + mode + '";' );
+	} );
+
+	grunt.registerTask( 'build', [
+		'default',
+		'usebanner',
+		'clean',
+		'copy',
+		'default', // Remove banners for GitHub
+	] );
+
+	grunt.registerTask( 'publish', ( releaseType ) => {
+		releaseType = releaseType ? releaseType : 'patch';
+
+		var prevStableVersion = 'patch' === releaseType ? pkgInfo.prev_stable_version : pkgInfo.version;
+
+		grunt.config.set( 'prev_stable_version', prevStableVersion );
+
+		grunt.task.run( 'default' );
+		grunt.task.run( 'bumpup:' + releaseType );
+		grunt.task.run( 'replace' );
+		grunt.task.run( 'shell:git_add_all' );
+		grunt.task.run( 'release' );
+	} );
+
+	grunt.registerTask( 'test', [
+		'qunit',
+		'clean:qunit',
+	] );
+};
---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+const ExtractText = require( 'extract-text-webpack-plugin' );
+const UglifyJsPlugin = require( 'uglifyjs-webpack-plugin' );
+const debug = process.env.NODE_ENV !== 'production';
+const path = require( 'path' );
+
+// const extractEditorSCSS = new ExtractText( {
+// 	filename: './blocks.editor.build.css',
+// } );
+
+const plugins = [ UglifyJsPlugin ];//extractEditorSCSS ];
+
+const externals = {
+	react: 'React',
+};
+
+const wpDependencies = [
+	'components',
+	'element',
+	'blocks',
+	'utils',
+	'date',
+	'data',
+	'i18n',
+	'editPost',
+	'plugins',
+	'apiRequest',
+	'editor',
+	'compose',
+];
+
+wpDependencies.forEach( wpDependency => {
+	externals[ '@wordpress/' + wpDependency ] = {
+		this: [ 'wp', wpDependency ],
+	};
+} );
+
+// const scssConfig = {
+// 	use: [
+// 		{
+// 			loader: 'css-loader',
+// 		},
+// 		{
+// 			loader: 'sass-loader',
+// 			options: {
+// 				outputStyle: 'compressed',
+// 			},
+// 		},
+// 	],
+// };
+
+const moduleRules = {
+	rules: [
+		{
+			enforce: 'pre',
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: 'eslint-loader',
+		},
+		{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			use: [
+				{
+					loader: 'babel-loader',
+					query: {
+						presets: [ '@wordpress/default' ],
+						plugins: [
+							[ 'transform-react-jsx', {
+								pragma: 'wp.element.createElement',
+							} ],
+						],
+					},
+				},
+			],
+		},
+		// {
+		// 	test: /editor\.scss$/,
+		// 	exclude: /node_modules/,
+		// 	use: extractEditorSCSS.extract( scssConfig ),
+		// },
+	],
+};
+
+const entryPoints = {
+	'template-block': __dirname + '/assets/dev/js/template-block.js',
+	'template-block.min': __dirname + '/assets/dev/js/template-block.js',
+};
+
+module.exports = {
+	context: __dirname,
+	devtool: debug ? 'inline-sourcemap' : null,
+	mode: debug ? 'development' : 'production',
+	entry: entryPoints,
+	output: {
+		path: path.resolve( __dirname, './assets/js' ),
+		filename: '[name].js',
+	},
+	module: moduleRules,
+	externals,
+	//plugins: plugins,
+};
