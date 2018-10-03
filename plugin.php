b06bf9f4ca362785afce88b650ebe202051d6689
raz ohad <admin@bainternet.info>
2018-10-03 16:21:14 +0300

    removed widget scripts and added autoloader

--- plugin.php
+++ plugin.php
@@ -28,6 +28,13 @@ class Plugin {
 	protected $blocks = [];
 
 	/**
+	 * class aliases
+	 * @access private
+	 * @var array
+	 */
+	private $classes_aliases = [];
+
+	/**
 	 * Instance
 	 *
 	 * Ensures only one instance of the class is loaded or can be loaded.
@@ -55,16 +62,6 @@ class Plugin {
 	}
 
 	/**
-	 * widget_scripts
-	 *
-	 * Load required plugin core files.
-	 * @access public
-	 */
-	public function widget_scripts() {
-		wp_register_script( 'elementor-hello-world', plugins_url( '/assets/js/hello-world.js', __FILE__ ), [ 'jquery' ], false, true );
-	}
-
-	/**
 	 * @param $template
 	 *
 	 * @access public
@@ -84,6 +81,40 @@ class Plugin {
 		$this->add_block( new Blocks\Template_Block() );
 	}
 
+	public function autoload( $class ) {
+		if ( 0 !== strpos( $class, __NAMESPACE__ ) ) {
+			return;
+		}
+
+		$has_class_alias = isset( $this->classes_aliases[ $class ] );
+
+		// Backward Compatibility: Save old class name for set an alias after the new class is loaded
+		if ( $has_class_alias ) {
+			$class_alias_name = $this->classes_aliases[ $class ];
+			$class_to_load = $class_alias_name;
+		} else {
+			$class_to_load = $class;
+		}
+
+		if ( ! class_exists( $class_to_load ) ) {
+			$filename = strtolower(
+				preg_replace(
+					[ '/^' . __NAMESPACE__ . '\\\/', '/([a-z])([A-Z])/', '/_/', '/\\\/' ],
+					[ '', '$1-$2', '-', DIRECTORY_SEPARATOR ],
+					$class_to_load
+				)
+			);
+			$filename = BLOCK_BUILDER_PATH . $filename . '.php';
+
+			if ( is_readable( $filename ) ) {
+				include( $filename );
+			}
+		}
+
+		if ( $has_class_alias ) {
+			class_alias( $class_alias_name, $class );
+		}
+	}
 
 	/**
 	 *  Plugin class constructor
@@ -93,6 +124,7 @@ class Plugin {
 	 * @access public
 	 */
 	public function __construct() {
+		spl_autoload_register( [ $this, 'autoload' ] );
 		add_filter( 'template_include', [ $this, 'template_include' ], 12 /* After WooCommerce & Elementor Pro - Locations Manager */ );
 		add_action( 'init', [ $this, 'register_blocks' ] );
 	}
