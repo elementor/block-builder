c3b16ad3a61fcd7ba0c18bf303f1a9c26d741292
Raz Ohad <ohad@elementor.com>
2023-11-14 17:32:31 +0200

    Fix: "This block has encountered an error ..." due to a11y update (#8)

--- src/template-block/components/placeholder.js
+++ src/template-block/components/placeholder.js
@@ -1,48 +1,20 @@
-import { Component, createElement } from '@wordpress/element';
 import { Placeholder } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 import ElementorIcon from './elementor-icon';
 
-const selectString = __( 'Select a template from your library or', 'block-builder' ) + ' ',
-	createString = __( 'create a new one.', 'block-builder' );
-
-const getInstructions = ( withLink = false ) => {
-	return withLink ? (
-		<div>
-			{ selectString }
-			<a
-				target={ '_blank' }
-				href={ elementorBlockBuilderConfig.create_new_post_url }
-			>
-				{ createString }
-			</a>
-		</div> ) :
-		(
-			selectString + createString
-		);
-}
-
 const ElementorPlaceholder = ( props ) => {
 	const {
 		withLink = false,
-		instructions = false,
+		instructions = '',
 		...restProps
 	} = props;
-
-	let renderInstructions;
-
-	if ( instructions ) {
-		renderInstructions = instructions;
-	} else {
-		renderInstructions = getInstructions( withLink );
-	}
 
 	return (
 		<Placeholder
 			icon={ <ElementorIcon color={ '#93003F' } /> }
-			label={ __( 'Elementor Library', 'block-builder' ) }
-			instructions={ renderInstructions }
+			label={ 'Elementor Library' }
+			instructions={ instructions }
 			{ ...restProps }
 		/>
 	);
